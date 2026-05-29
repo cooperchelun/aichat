61525cdd0dc8b6e00b14422fb575c02ea6820487
cooperchelun <cooperchelun@gmail.com>
2026-05-29 14:12:33 +0800

    Update web.py

--- web.py
+++ web.py
@@ -16,3 +16,70 @@ import os
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
+
+# 判斷是在 Vercel 還是本地
+if os.path.exists('serviceAccountKey.json'):
+    # 本地環境：讀取檔案
+    cred = credentials.Certificate('serviceAccountKey.json')
+else:
+    # 雲端環境：從環境變數讀取 JSON 字串
+    firebase_config = os.getenv('FIREBASE_CONFIG')
+    cred_dict = json.loads(firebase_config)
+    cred = credentials.Certificate(cred_dict)
+
+firebase_admin.initialize_app(cred)
+
+
+app = Flask(__name__)
+
+client = genai.Client()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/messenger")
+def messenger():
+    return render_template("messenger.html")
+
+@app.route("/AI")
+def AI():
+    # 每次使用者拜訪該路徑時，直接使用全域的 client 呼叫模型
+    response = client.models.generate_content(
+        model='gemini-3.1-flash-lite',
+        contents='我想查詢靜宜大學資管系的評價？',
+    )
+    
+    # 回傳生成的文字
+    return response.text
+
+
+@app.route('/ask', methods=['GET', 'POST']) 
+def ask():
+    if request.method == "POST":
+        user_prompt = request.form.get('prompt', '')
+        if not user_prompt:
+            return "請輸入內容", 400
+        try:
+            response = client.models.generate_content(
+                model='gemini-3.5-flash',
+                contents=user_prompt,
+            )
+            return response.text
+        except Exception as e:
+            return f"發生錯誤: {str(e)}", 500
+
+    else:    
+        # 當使用者直接打開網頁 (GET) 時，顯示輸入框畫面
+        return render_template("ask.html")
+
+
+
+@app.route("/message")
+def message():
+    return render_template("message.html")
+
+
+@app.route("/webdemo")
+def webdemo():
+    return render_template("webdemo.html")
