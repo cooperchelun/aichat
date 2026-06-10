f65534a8e229c1ce2194aaed8fb2d311e5165160
cooperchelun <cooperchelun@gmail.com>
2026-06-10 19:40:46 +0800

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,10 +1,47 @@
+const axios = require("axios");
+
 module.exports = async (req, res) => {
-  console.log("ENV CHECK:", {
-    token: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-    secret: process.env.LINE_CHANNEL_SECRET
-  });
+  try {
 
-  console.log("BODY:", req.body);
+    const body =
+      typeof req.body === "string"
+        ? JSON.parse(req.body)
+        : req.body;
 
-  return res.status(200).send("debug ok");
+    const event = body?.events?.[0];
+
+    if (!event) return res.status(200).send("OK");
+
+    const text = event.message.text;
+
+    const list = await axios.get(
+      "https://genshin.jmp.blue/characters"
+    );
+
+    const found = list.data.find(
+      c => c.toLowerCase() === text.toLowerCase()
+    );
+
+    if (!found) {
+      return res.status(200).json({
+        reply: "找不到角色（輸入 furina）"
+      });
+    }
+
+    const detail = await axios.get(
+      `https://genshin.jmp.blue/characters/${found}`
+    );
+
+    return res.status(200).json({
+      reply:
+`角色：${detail.data.name}
+元素：${detail.data.vision}
+武器：${detail.data.weapon}
+稀有度：${detail.data.rarity}★`
+    });
+
+  } catch (err) {
+    console.error(err);
+    return res.status(200).send("handled error");
+  }
 };
